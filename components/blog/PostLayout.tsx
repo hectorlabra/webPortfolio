cf87477a4c8640ff5a6c661aa059ad210ff219ee
Hector Labra <hlabrablistavap@gmail.com>
2025-11-06 20:25:37 -0300

    fix(blog): actually center article at 700px like quien-soy
    Add max-w-[700px] mx-auto to article element.
    This centers content properly within the 1fr grid column.
    
    Now matches quien-soy width exactly.

--- components/blog/PostLayout.tsx
+++ components/blog/PostLayout.tsx
@@ -124,7 +124,7 @@ export function PostLayout({
           </aside>
 
           {/* Contenido principal - Centered */}
-          <article className="min-w-0 w-full">
+          <article className="min-w-0 w-full max-w-[700px] mx-auto">
             {/* Contenido del post */}
             <div
               className="prose prose-invert max-w-none
